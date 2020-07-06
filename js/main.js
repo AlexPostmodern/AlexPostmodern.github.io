8f8498d07d912d563dce2fd45bfaf2194cee4ae0
AlexPostmodern <postnickov.lyoha@yandex.ru>
2020-07-06 20:27:17 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ buttonHome.onclick = () => {
   }
   body.style.background = "linear-gradient(90deg, #aea4e3, #d3ffe8) fixed"
 }
+
 buttonAbout.onclick = () => {
   if (social.style.getPropertyValue("--color-social") == "#fff") {
     social.style.setProperty("--color-social", "rgb(83, 83, 83)")
@@ -24,6 +25,7 @@ buttonAbout.onclick = () => {
   }
   body.style.background = "linear-gradient(90deg, #cfecd0, #a0c9ce, #9ec0db) fixed"
 }
+
 buttonProjects.onclick = () => {
   if (social.style.getPropertyValue("--color-social") == "#fff") {
     social.style.setProperty("--color-social", "rgb(83, 83, 83)")
@@ -31,11 +33,13 @@ buttonProjects.onclick = () => {
   }
   body.style.background = "linear-gradient(to right, #fff7a3, #ffe0a3, #ffccb7, #ffc1d3, #fbbeeb, #e7bcf2, #d0bbf6, #b6baf7, #afb4f8, #a9aff8, #a2a9f9, #9ba4f9) fixed"
 }
+
 buttonHobby.onclick = () => {
   footer.style.color = "#fff"
   social.style.setProperty("--color-social", "#fff")
   body.style.background = "linear-gradient(90deg, #9c8de4, #ad4454) fixed"
 }
+
 buttonContact.onclick = () => {
   footer.style.color = "#fff"
   social.style.setProperty("--color-social", "#fff")
