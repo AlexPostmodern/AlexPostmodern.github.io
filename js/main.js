d6a6d9237260445444ffad7c5891619ffdb50782
AlexPostmodern <postnickov.lyoha@yandex.ru>
2020-07-06 17:32:28 +0300

    +sections

--- js/main.js
+++ js/main.js
@@ -22,11 +22,11 @@ function toggleTheme() {
 document.getElementById('themeButton').onclick = toggleTheme;*/
 
 const body=document.querySelector("body")
-const buttonHome = document.getElementById("home")
-const buttonAbout = document.getElementById("about")
-const buttonProjects = document.getElementById("projects")
-const buttonHobby = document.getElementById("hobby")
-const buttonContact = document.getElementById("contact")
+const buttonHome = document.getElementById("home-btn")
+const buttonAbout = document.getElementById("about-btn")
+const buttonProjects = document.getElementById("projects-btn")
+const buttonHobby = document.getElementById("hobby-btn")
+const buttonContact = document.getElementById("contact-btn")
 
 //buttonHome.on
 
@@ -37,11 +37,11 @@ buttonAbout.onclick = ()=>{
   body.style.background="linear-gradient(90deg, #cfecd0, #a0c9ce, #9ec0db) fixed"
 }
 buttonProjects.onclick = ()=>{
-  body.style.background="linear-gradient(to right, #fff7a3, #ffe0a3, #ffccb7, #ffc1d3, #fbbeeb, #e7bcf2, #d0bbf6, #b6baf7, #afb4f8, #a9aff8, #a2a9f9, #9ba4f9)fixed"
+  body.style.background="linear-gradient(to right, #fff7a3, #ffe0a3, #ffccb7, #ffc1d3, #fbbeeb, #e7bcf2, #d0bbf6, #b6baf7, #afb4f8, #a9aff8, #a2a9f9, #9ba4f9) fixed"
 }
 buttonHobby.onclick = ()=>{
   body.style.background="linear-gradient(90deg, #9c8de4, #ad4454) fixed"
 }
 buttonContact.onclick = ()=>{
-  body.style.background="linear-gradient(to left bottom, #370512, #592042, #6a457d, #6071b9, #12a0eb) fixed"
+  body.style.background="linear-gradient(to left, #370512, #592042, #6a457d, #6071b9, #12a0eb) fixed"
 }
